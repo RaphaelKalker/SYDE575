918629470e3cb0370f71622e31e4aaaa2470ec26
Raphael <raphael.kalker@gmail.com>
2015-09-30 17:59:37 -0400

    psnr had wrong max value

--- lab1/PSNR.m
+++ lab1/PSNR.m
@@ -1,8 +1,10 @@
 function PSNR_out = PSNR(f, g) %f = reference image, g = test image
-[m, n] = size(g);
-diffSquared = power(f-g,2);
-MSE = 1/(m*n)*sum(diffSquared(:));
-PSNR_out = 10*log10(power(max(m, n),2)/MSE);
+    MAX = 256; %0-255 means 256 values
+    [m, n] = size(g);
+    
+    diffSquared = (double(f)-double(g)).^2;
+    MSE = 1/(m*n)*sum(diffSquared(:));
+    
+    psnr(f,g)
+    PSNR_out = 10*log10(power(MAX,2)/MSE)
 end
-
-
